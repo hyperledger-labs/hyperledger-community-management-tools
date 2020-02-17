86e2c1f52cc8e0494cff4e34665ba0a09c2c0da8
Danno Ferrin <danno.ferrin@gmail.com>
2020-02-17 15:16:03 -0700

    Spec based proposal
    This aims to make a spec based proposal that also addresses the
    feedbak given to prior proposals and to the specifics of the repolinter
    that were not objected to by anyone else.
    
    I also updated the repolinter to make it run against all projects.  Some
    hackery with copying the repolinter into the local cloned repo was
    required as repolinter does not allow pointing to an external config.
    
    Required content is not automatically checked, but is part of the spec.
    
    Signed-off-by: Danno Ferrin <danno.ferrin@gmail.com>

---
+++ repo_structure/repo_structure.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+#Default variables
+repositories=()
+filename="hyperledger-repository-structure"
+since=""
+until=""
+output_dir=/tmp
+INCLUDE_PROJECT_OPTIONS=1
+INCLUDE_DATE_OPTIONS=1
+short_description="Reports on conformance to the Hyperledger Repository Structure."
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+script_dir="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+. ${script_dir}/../common/command-line.sh
+
+today=`date -u +%Y-%m-%d-%H-%M-%S`
+outdir="${output_dir}"/${filename}-${today}
+outfile="${outdir}"/repo_structure.txt
+mkdir -p "${outdir}"/working
+
+srcdir=/tmp/${filename}-${today}
+mkdir -p ${srcdir}/source
+cd ${srcdir}/source
+
+for i in ${repositories[@]};
+do
+  echo "Processing $i..."
+  cd ${srcdir}/source
+  git clone --single-branch $i
+  repo=`basename -s .git $i`
+  cd ${repo}
+  echo >> ${outfile}
+  echo "Report for $i" >> ${outfile}
+
+  if [ -h "repolint.json" ]; then
+    echo custom repolint.json exists: >> ${outfile}
+    diff repolint.json ${script_dir}/repolint.json >> ${outfile}
+  fi
+  cp ${script_dir}/repolint.json .
+  npx repolinter >> ${outfile}
+  if [ ?$ ]; then
+    echo "FAILED" >> ${outfile}
+  fi
+done
+
+
+rm -fr ${srcdir}/source
