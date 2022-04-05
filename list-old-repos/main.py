8b46f2bc3c6446116851e25d4abc1b1c87b51b7b
Tracy Kuhrt <tracy.a.kuhrt@accenture.com>
2022-04-05 15:59:35 -0700

    Initial version
    Signed-off-by: Tracy Kuhrt <tracy.a.kuhrt@accenture.com>

---
+++ list-old-repos/main.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+# SPDX-License-Identifier: Apache-2.0
+
+import time
+import getpass
+from jinja2 import Template
+import dateutil.parser
+import datetime
+from datetime import timezone
+import requests
+import argparse
+
+
+def get_api_results(url, username, password, paginate=False):
+    print("Processing URL: %s" % url)
+    while True:
+        r = requests.get(url, auth=(username, password))
+
+        # This API sometimes returns a 202, so we want to make sure that we get the
+        # results before moving on
+        if r.status_code == 202:
+            time.sleep(1)
+            continue
+
+        r.raise_for_status()
+        break
+
+    results = r.json()
+
+    if paginate and 'next' in r.links.keys():
+      results.extend(get_api_results(r.links['next']['url'], username, password)) 
+
+    return results
+
+
+def get_non_archived_repositories_for_org(gh_org, username, password):
+    t = Template("https://api.github.com/orgs/{{org}}/repos?page=1")
+    url = t.render(org=gh_org)
+    results = get_api_results(url, username, password, paginate=True)
+
+    repositories = sorted(set(map(lambda y: y['full_name'], (list(filter(lambda x:x['archived'] == False, results))))))
+
+    return repositories
+
+
+def get_last_commit_date(gh_repo, username, password):
+    t = Template("https://api.github.com/repos/{{repo}}/commits/HEAD")
+    url = t.render(repo=gh_repo)
+    results = get_api_results(url, username, password)
+
+    return results['commit']['committer']['date']
+
+def list_old_repositories(org, username, password, when, details):
+    repos = get_non_archived_repositories_for_org(org, username, password)
+
+    old_repos = []
+
+    for r in repos:
+        last_commit_date = dateutil.parser.isoparse(get_last_commit_date(r, username, password))
+        if last_commit_date <= when.replace(tzinfo=timezone.utc):
+            if details:
+                old_repos.append("{},{}".format(r, last_commit_date))
+            else:
+                old_repos.append(r)
+
+    for r in old_repos:
+        print(r)
+
+def main():
+    parser = argparse.ArgumentParser(description="List repositories that have not had a commit in a certain time period.")
+    parser.add_argument("-u", "--username",
+        help="Github username to use for API calls (required)",
+        required=True)
+    parser.add_argument("-p", "--password",
+        help="Github access token to use for API calls (required)",
+        required=False)
+    parser.add_argument("-o", "--org", help="GitHub organization",
+        required=True)
+    parser.add_argument("-d", "--details", help="Include last commit date in output.",
+        default=False, action='store_true')
+    parser.add_argument("-w", "--when", help="Include repos with last commit prior to date (%Y-%m-%d).",
+        required=True, type=lambda s: datetime.datetime.strptime(s, '%Y-%m-%d'))
+    args = parser.parse_args()
+
+    username = args.username
+    password = args.password
+
+    if not password:
+        password = getpass.getpass("Please enter github access token:")
+
+    list_old_repositories(args.org, username, password, args.when, args.details)
+
+
+if __name__ == "__main__":
+    main()
